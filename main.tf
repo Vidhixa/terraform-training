b586c0b38110b32ff80abcfe3c04ad1d7ebe84c3
HashiCorp Training <training@hashicorp.com>
2017-04-12 17:00:32 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+# NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-eea9f38e
+#
+# Your subnet ID is:
+#
+#     subnet-7e08481a
+#
+# Your security group ID is:
+#
+#     sg-834d35e4
+#
+# Your Identity is:
+#
+#     autodesk-rabbit
+#
+terraform {
+  backend "atlas" {
+    name = "vidhixa/vid"
+  }
+}
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "us-west-1"
+}
+
+variable "total_count" {
+  type = "string"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-eea9f38e"
+  subnet_id              = "subnet-7e08481a"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-834d35e4"]
+
+  tags {
+    Identity       = "autodesk-rabbit"
+    Cloud_Provider = "AWS"
+    Use_Type       = "training"
+    Name           = "web ${count.index+1}/${var.total_count}"
+  }
+
+  count = "${var.total_count}"
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
