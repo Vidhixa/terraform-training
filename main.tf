e2b3d7cb23fcdfb9947a89e161903889ab4d896d
Vidhixa Joshi <Vidhixa@users.noreply.github.com>
2017-04-12 11:23:41 -0700

    Adding syntax error for exercise

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ variable "total_count" {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
+  access_key = ${var.aws_access_key}
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 }
